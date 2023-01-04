833aa82a96bf0e6fa86a6a4f49cbba383ddf8e08
ChrisPlease <chris@chrisplease.me>
2023-01-03 21:12:37 -0700

    everything is working, but not pretty

--- src/assets/scripts/collapsible-panel.ts
+++ src/assets/scripts/collapsible-panel.ts
@@ -1,9 +1,12 @@
+function ease (v: number, pow = 4): number {
+  return 1 - Math.pow(1 - v, pow)
+}
+
 export class CollapsiblePanel {
   private readonly _el: HTMLElement
   private readonly _toggle: HTMLElement
   private readonly _content: HTMLElement
 
-  private panelHeight = 0
   isOpen = false
 
   constructor (element: HTMLElement) {
@@ -17,42 +20,87 @@ export class CollapsiblePanel {
     return this._el.querySelector(getter)!
   }
 
+  private clickHandler (e: Event): void {
+    if (e.target && Array.from<EventTarget>(this.links).includes(e.target)) {
+      this.collapsePanel()
+    }
+  }
+
+  animateClosed (): void {
+    const foo = new Array<number>(100).fill(1).map((_, i) => {
+      const step = i
+      const easedStep: number = 1 + (-1 * ease(step / 100))
+      return `scale(1, ${easedStep})`
+    })
+
+    const bar = new Array<number>(100).fill(1).map((_, i) => {
+      const step = i
+      const easedStep: number = 1 / (1 + (-1 * ease(step / 100)))
+      return `scale(1, ${easedStep})`
+    })
+
+    this._content.animate({
+      transform: foo,
+    }, { duration: 1000 })
+
+    this._content.querySelector('nav')?.animate({
+      transform: bar,
+    }, { duration: 1000 })
+  }
+
+  animateOpen (): void {
+    const foo = new Array<number>(100).fill(1).map((_, step) => {
+      const easedStep = ease(step / 100)
+      return `scale(1, ${easedStep})`
+    })
+
+    const bar = new Array<number>(100).fill(1).map((_, step) => {
+      const easedStep = 1 / ease(step / 100)
+      return `scale(1, ${easedStep})`
+    })
+
+    this._content.animate({
+      transform: foo,
+    }, { duration: 1000 })
+
+    this._content.querySelector('nav')?.animate({
+      transform: bar,
+    }, { duration: 1000 })
+  }
+
   collapsePanel (): void {
-    this._content.style.height = '0px'
+    this.animateClosed()
     this._toggle.classList.remove('is-open')
+    this._content.classList.remove('is-open')
     this._toggle.setAttribute('aria-expanded', 'false')
     this.isOpen = false
+
+    this._content.removeEventListener('click', this.clickHandler)
   }
 
   expandPanel (): void {
-    this._content.style.height = `${this.panelHeight}px`
+    this.animateOpen()
     this._toggle.classList.add('is-open')
+    this._content.classList.add('is-open')
     this._toggle.setAttribute('aria-expanded', 'true')
     this.isOpen = true
+
+    this._content.addEventListener('click', this.clickHandler)
   }
 
-  togglePanel (): void {
+  togglePanel (e: Event): void {
+    e.preventDefault()
     if (this.isOpen) { this.collapsePanel() } else { this.expandPanel() }
   }
 
-  setPanelSize (): void {
-    this.panelHeight = this._content.getBoundingClientRect().height
-  }
-
-  get links (): NodeList {
-    return this._content.querySelectorAll('a')
+  get links (): HTMLElement[] {
+    return Array.from(this._content.querySelectorAll('a'))
   }
 
   init (): void {
-    this.setPanelSize()
-
     this._toggle.setAttribute('aria-haspopup', 'menu')
-    this._toggle.setAttribute('aria-expanded', 'true')
+    this._toggle.setAttribute('aria-expanded', 'false')
 
-    if (!this.isOpen) {
-      this.collapsePanel()
-    }
-
-    this._toggle.addEventListener('click', () => this.togglePanel())
+    this._toggle.addEventListener('click', (e) => this.togglePanel(e), false)
   }
 }
